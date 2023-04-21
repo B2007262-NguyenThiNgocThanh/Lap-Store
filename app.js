d76f749aedf8a4aece3d6cc065a1e6f6113cd86e
Qing31 <nguyenthingocthanh31012002.gmail>
2023-04-21 21:25:42 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const productRouter = require("./app/routes/product.route");
 const staffRouter = require("./app/routes/staff.route");
@@ -15,9 +16,25 @@ app.use("/api/category", categoryRouter);
 app.use("/api/shift", shiftRouter);
 
 
+//handle 404 response
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found")); 
+});
+
+//define error-handling middleware last, after other app.use() and route calls
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Interal Server Error",
+    });
+});
+
+
+
 app.get("/", (req, res) =>{
     res.json({message: "Welcome to management laptop store application."});
 });
 
 module.exports = app;
+
+
 
